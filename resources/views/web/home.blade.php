a1b75e291a8da1054b27f1ca59232d5160a56148
rohit99336 <rohit99336@gmail.com>
2023-09-12 17:43:48 +0530

    add lang changer

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -751,6 +751,13 @@
             });
         });
     </script>
+    <script type="text/javascript">
+        var url = "{{ route('changeLang') }}";
+
+        $(".changeLang").change(function() {
+            window.location.href = url + "?lang=" + $(this).val();
+        });
+    </script>
 
 </body>
 
