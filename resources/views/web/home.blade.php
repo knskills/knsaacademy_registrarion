db5f6e922c5efdd6d1f35cd88934306954be2212
rohit99336 <rohit99336@gmail.com>
2023-12-09 15:10:20 +0530

    dsfds

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -340,7 +340,7 @@
                             </a>
                         </div><br>
 
-                        {{-- <div class="d-flex justify-content-center">
+                        <div class="d-flex justify-content-center">
                             <div id="timer">
                                 <b class="text-dark">Offer Expired:</b>
                                 <div class="hours">
@@ -356,7 +356,7 @@
                                     <span class="unit">SECS</span>
                                 </div>
                             </div>
-                        </div> --}}
+                        </div>
                     </div>
                 </div>
             </div>
