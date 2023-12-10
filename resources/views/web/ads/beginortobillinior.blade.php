5a2019c2798b1e7e70c13e7f108855ec3249d497
rohit99336 <rohit99336@gmail.com>
2023-12-10 18:11:10 +0530

    test

--- resources/views/web/ads/beginortobillinior.blade.php
+++ resources/views/web/ads/beginortobillinior.blade.php
@@ -12,18 +12,18 @@
     <section id="hero" class="d-flex align-items-center">
         <div class="container" data-aos="zoom-out" data-aos-delay="100" id="main_t">
             <div class="row">
-                {{-- <div class="col-12 text-center mb-3">
+                <div class="col-12 text-center mb-3">
                     <h2 class="text-black" style="height: 10%">
                         From Novice to Nine Figures: The Journey to Billionaire Status
                     </h2>
-                </div> --}}
+                </div>
 
                 <div class="col-md-6 justify-content-center">
                     <h1>Mastering the art and science of wealth creation with <span>Kamal Narayan Sahu</span></h1>
                     <h2>From Beginner to Billionaire: Kamal Narayan Sahu's Network Marketing Journey</h2>
                     <div class="text-center m-auto mt-4">
                         <a href="#skills" class="btn-custom mb-3" {{ $disabled }}>
-                            <span>Book Now Your Seat <b>₹99</b> </span>
+                            <span>Book Now Your Seat <b>₹199</b> </span>
                         </a>
                     </div><br>
 
