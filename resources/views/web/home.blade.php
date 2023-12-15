b1b9d33e63314ad059fff03c5d725bc9135562de
rohit99336 <rohit99336@gmail.com>
2023-12-15 19:43:29 +0530

    add files

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1075,6 +1075,31 @@
         // Initial call to set the timer immediately
         updateCountdown();
     </script> --}}
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '365953992779410');
+        fbq('track', 'PageView');
+    </script>
+
+
     <script>
         // Set the target date and time (1 hour from the current time)
         const targetDate = new Date();
