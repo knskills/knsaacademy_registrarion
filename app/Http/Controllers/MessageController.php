9406c04ea28743c74386cc9075aecc667b66bac1
rohit99336 <rohit99336@gmail.com>
2024-01-09 21:12:46 +0530

    add helper and move send functions

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -62,89 +62,13 @@ class MessageController extends Controller
             // $message = str_replace("{time}", $audience->event->time, $message);
 
             if ($request->type == 'whatsapp') {
-                $reasult = $this->sendWhatsAppMessage($audience->phone, $message);
+                $reasult = sendWhatsAppMessage($audience->phone, $message);
             } else {
-                $reasult = $this->sendSms($audience->phone, $message);
+                $reasult = sendSms($audience->phone, $message);
             }
         }
         return redirect()->route('messages.index')->with('success', 'Message created successfully');
     }
 
-    public function sendSms($phone, $message)
-    {
-        try {
-            $mobileNumber = $phone;
-            $message =  $message;
-            $senderId = getenv("MSGCLUB_SENDER_ID");
-            $serverUrl = getenv("MSGCLUB_SERVER_URL");
-            $authKey = getenv("MSGCLUB_AUTH_KEY");
-            $routeId = getenv("MSGCLUB_SMS_ROUTE");
-            $this->sendsmsGET($mobileNumber, $senderId, $routeId, $message, $serverUrl, $authKey);
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
-        }
-    }
 
-    public function sendsmsGET($mobileNumber, $senderId, $routeId, $message, $serverUrl, $authKey)
-    {
-        $route = $routeId;
-        $getData = 'mobileNos=' . $mobileNumber . '&message=' . urlencode($message) . '&senderId=' . $senderId . '&routeId=' . $route;
-
-        /* API URL */
-        $url = "http://" . $serverUrl . "/rest/services/sendSMS/sendGroupSms?AUTH_KEY=" . $authKey . "&" . $getData;
-
-        Log::info($url);
-
-        /* init the resource */
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYHOST => 0,
-            CURLOPT_SSL_VERIFYPEER => 0
-        ));
-
-        /* get response */
-        $output = curl_exec($ch);
-
-        /* Print error if any */
-        if (curl_errno($ch)) {
-            echo 'error:' . curl_error($ch);
-        }
-
-        curl_close($ch);
-
-        Log::info($output);
-
-        return $output;
-    }
-
-    public function sendWhatsAppMessage($phone, $message)
-    {
-        $authKey = getenv("MSGCLUB_AUTH_KEY");
-        $whatsappNumber = getenv("MSGCLUB_AWHATSAPP_NO");
-        $toNumber = $phone; // Replace with the recipient's WhatsApp number
-        $bodyText = $message; // Replace with the message body text
-
-        $url = 'http://msg.msgclub.net/rest/services/sendSMS/v2/sendtemplate?AUTH_KEY=' . $authKey;
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Cookie' => 'JSESSIONID=23BD7D8B4F08B438F9A42E5334C2DDEB.node3',
-        ])->post($url, [
-            'senderId' => $whatsappNumber,
-            'component' => [
-                'messaging_product' => 'whatsapp',
-                'recipient_type' => 'individual',
-                'to' => $toNumber,
-                'type' => 'text',
-                'text' => [
-                    'preview_url' => true,
-                    'body' => $bodyText,
-                ],
-            ],
-        ]);
-
-        return $response->body();
-    }
 }
