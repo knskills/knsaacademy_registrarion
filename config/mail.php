cfadc61efde5780b08fb806aeeb6625451584555
rohit99336 <rohit99336@gmail.com>
2024-02-03 11:37:29 +0530

    update conf

--- config/mail.php
+++ config/mail.php
@@ -35,9 +35,9 @@ return [
 
     'mailers' => [
         'smtp' => [
-            // 'transport' => 'mail', // change smtp to mail for hostinger mail
+            'transport' => 'mail', // change smtp to mail for hostinger mail
             // 'transport' => 'smtp', // change smtp to mail for local mail
-            'transport' => env('MAIL_MAILER', 'mail'),
+            // 'transport' => env('MAIL_MAILER', 'mail'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
