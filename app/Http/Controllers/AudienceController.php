e4d1f868a977ca3013c85e5181ac71cbf083914b
rohit99336 <rohit99336@gmail.com>
2024-05-08 17:48:29 +0530

    add changes

--- app/Http/Controllers/AudienceController.php
+++ app/Http/Controllers/AudienceController.php
@@ -60,7 +60,7 @@ class AudienceController extends Controller
      */
     public function store(Request $request)
     {
-        Log::info($request->all());
+        // Log::info($request->all());
 
         try {
             $valitor = Validator::make($request->all(), [
@@ -115,7 +115,7 @@ class AudienceController extends Controller
                 //     sendSms($audience->phone, $message);
                 // }
             }else{
-                Log::info('Audience already exists');
+                // Log::info('Audience already exists');
                 // update event name and registration date
                 $audience->event_name = $request->event_name;
                 $audience->registration_date = Carbon::now();
