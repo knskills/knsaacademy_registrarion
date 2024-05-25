408d3cc2830df06ecd4382c5f9180ba532a1fe16
rohit99336 <rohit99336@gmail.com>
2024-05-25 13:48:01 +0530

    update code

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\WhatsappMessage;
+use App\Events\MessageReceived;
 
 class WebhookController extends Controller
 {
@@ -12,7 +13,7 @@ class WebhookController extends Controller
 
     public function setupWebhook(Request $request)
     {
-        // Log::info('call webhook setup');
+        Log::info('call webhook setup');
 
         if ($request->isMethod('get')) {
             return $this->handleGetRequest($request);
@@ -27,9 +28,13 @@ class WebhookController extends Controller
     {
         $hubVerifyToken = $request->query('hub_verify_token');
 
+        Log::info('Verify token: ' . $hubVerifyToken);
+
         if ($hubVerifyToken !== self::VERIFY_TOKEN) {
             return response()->json(['error' => 'VerifyToken doesnt match'], 403);
         }
+
+        Log::info($request->query('hub_challenge'));
 
         return response($request->query('hub_challenge'));
     }
@@ -96,6 +101,9 @@ class WebhookController extends Controller
                 'from' => $message['from'] ?? null,
             ]);
         }
+
+        // // Broadcast the event with the new message data
+        // broadcast(new MessageReceived($data));
 
         return end($messages)['from'];
     }
