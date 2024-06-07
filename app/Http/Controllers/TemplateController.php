4d7a99225100f5de5061e39d7cc0057e974a0b5e
rohit99336 <rohit99336@gmail.com>
2024-06-07 15:56:35 +0530

    update code

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -70,7 +70,7 @@ class TemplateController extends Controller
             $whtsp_msg = [];
 
             if ($request->type == 'whatsapp') {
-                $res = getMessageTemplate($request->name, $imageName = null);
+                $res = getMessageTemplate($request->name, $$path = null);
                 if (!$res) {
                     return redirect()->back()->with('error', 'Template not found!');
                 }
