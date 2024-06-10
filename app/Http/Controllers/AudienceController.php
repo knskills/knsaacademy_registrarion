1a23241dc664d3fb7e798797ff0c314aa87442b4
rohit99336 <rohit99336@gmail.com>
2024-06-10 17:20:17 +0530

    update temp

--- app/Http/Controllers/AudienceController.php
+++ app/Http/Controllers/AudienceController.php
@@ -133,7 +133,7 @@ class AudienceController extends Controller
                 $audience->registration_date = Carbon::now();
                 $audience->save();
 
-                $template = WhtasappTemplate::where('name', 'learn_nt_m')->first();
+                $template = WhtasappTemplate::where('name', 'registartion_confirmation')->first();
                 Log::info($template);
                 if ($template) {
                     sendTempMessage($template, $request->phone, $para = null);
