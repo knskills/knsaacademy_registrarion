bd53ed8ef75141ca2be7920d9f43ba093e64a3fe
rohit99336 <rohit99336@gmail.com>
2024-06-12 12:53:29 +0530

    change

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -442,7 +442,7 @@
                                                                                 d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708" />
                                                                         </svg>
 
-                                                                    @elseif($message->status == 'read')
+                                                                    @elseif($message->status == 'failed')
                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(248, 40, 40)" class="bi bi-ban" viewBox="0 0 16 16">
                                                                             <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
                                                                         </svg>
