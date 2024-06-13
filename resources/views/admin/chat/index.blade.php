28d9cdcd7d11a38acafac72eda98af89032456c6
rohit99336 <rohit99336@gmail.com>
2024-06-13 19:14:25 +0530

    update code

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
-    {{-- @vite('resources/js/app.js') --}}
 
     <style>
         .custom-button-wrapper {
