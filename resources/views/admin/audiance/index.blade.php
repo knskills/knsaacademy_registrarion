69704c217e02f5f7943c4ad49ad8a2adb2aa75da
rohit99336 <rohit99336@gmail.com>
2024-06-17 18:40:07 +0530

    change string case

--- resources/views/admin/audiance/index.blade.php
+++ resources/views/admin/audiance/index.blade.php
@@ -130,7 +130,8 @@
                                                 <td>{{ $item->name }}</td>
                                                 <td>{{ $item->email }}</td>
                                                 <td>{{ $item->phone }}</td>
-                                                <td>{{ ucwords(str_replace('_', ' ', $item->event_name)) }}
+                                                <td>
+                                                    {{ ucwords(str_replace('_', ' ', $item->event_name)) }}
                                                 </td>
                                                 <td class="text-center">
                                                     {{ \Carbon\Carbon::parse($item->registration_date)->format('d-m-Y') }}
